032cc4606db3b739dd1a4333e8748a01db092525
Adrian Marin <adrian@adrianthedev.com>
2021-08-24 15:37:38 +0300

    refactor: manually add the notice tailwind classes

--- src/.vuepress/tailwind.config.js
+++ src/.vuepress/tailwind.config.js
@@ -11,10 +11,14 @@ module.exports = {
       ".vuepress/theme/**/*.vue",
       './0.4.x/**/*.md',
       './1.0/**/*.md',
+      './../0.4.x/**/*.md',
+      './../1.0/**/*.md',
+      '0.4.x/**/*.md',
+      '1.0/**/*.md',
     ],
     options: {
-      safelist: ['border'],
-      whitelist: ['border']
+      safelist: ['border', 'rounded-md', 'bg-blue-50', 'p-4', 'flex', 'flex-shrink-0', 'h-5', 'w-5', 'text-blue-400', 'ml-3', 'flex-1', 'md:flex', 'md:justify-between', 'text-sm', 'leading-5', 'text-blue-700', 'underline'],
+      whitelist: ['border', 'rounded-md', 'bg-blue-50', 'p-4', 'flex', 'flex-shrink-0', 'h-5', 'w-5', 'text-blue-400', 'ml-3', 'flex-1', 'md:flex', 'md:justify-between', 'text-sm', 'leading-5', 'text-blue-700', 'underline'],
     }
   },
   darkMode: false, // or 'media' or 'class'
